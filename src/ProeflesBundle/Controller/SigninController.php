201d3a3bce32dcda2a151e57e533d4c859416db6
DaanKooi <daankooi00@gmail.com>
2018-04-23 15:03:06 +0200

    Begonnen inschrijfformulier

---
+++ src/ProeflesBundle/Controller/SigninController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ProeflesBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Signin controller.
+ *
+ * @Route("signin")
+ */
+
+class SigninController extends Controller
+{
+    /**
+     * @Route("/", name="signin_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $locations = $em->getRepository('ProeflesBundle:Location')->findAll();
+
+        return $this->render('signin/index.html.twig', array(
+            'locations' => $locations,
+        ));
+    }
+}
