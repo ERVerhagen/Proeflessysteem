5e0236b8fc89d893abe8e33a8768be3c530523f2
daryl <daryl_holling@hotmail.com>
2018-04-24 09:23:02 +0200

    haal maar binnen

--- src/ProeflesBundle/Form/locatieType.php
+++ src/ProeflesBundle/Form/locatieType.php
@@ -5,6 +5,7 @@ namespace ProeflesBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class locatieType extends AbstractType
 {
@@ -13,7 +14,17 @@ class locatieType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('stad')->add('adres')->add('postcode')->add('img')->add('memo');
+        $builder
+            ->add('stad')
+            ->add('adres')
+            ->add('postcode')
+            ->add('img')
+            ->add('memo')
+            ->add('imagepath', FileType::class, [
+                'data_class' => null,
+                'empty_data' => $builder->getForm()->getData('locatie')->getImg(),
+                'label' => 'logo image file',
+                'required' => false]);;
     }/**
      * {@inheritdoc}
      */
